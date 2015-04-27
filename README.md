0ca42d3d7c531ace3e83c25268f26d133b016477
Pascal Rettig <pascal@cykod.com>
2015-04-26 21:42:26 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Collection.js
+
+Simple Collection filtering and searching API.
+
+# How to use
+
+Create a collection using a particular selector
+
+    var myCollection = $(".item").collection({
+      filters: {
+        // filter on text of a child member
+        "title": "h2",
+        // or the value of a attribute or data-attribute on the .item
+        "category": "[data-category]"
+      },
+
+      update: function() { 
+        // Called everytime something is filtered
+      },
+
+      // Optionally override hide and show functions
+
+      // hide: function($elem) { $elem.hide(); },
+      // show: function($elem) { $elem.fadeIn(500); },
+    });
+
+    // Call collection.filtered(filter,value) to filter the collection
+
+    $("#blue-button").on("click",function(e) {
+        myCollection.filtered("category","blue");
+    });
+
+    $("#red-button").on("click",function(e) {
+        myCollection.filtered("category","red");
+    });
+
+    $("#all-button").on("click",function(e) {
+        myCollection.filtered("category","");
+    });
+
+
+    
+
---
+++ collection.js
@@ -0,0 +1,108 @@
+(function(global) {
+
+  var collections = {};
+
+  var dataMatch = /^\[data\-([^\]]+)\]$/i;
+  var attrMatch =  /^\[([^\]]+)\]$/i;
+
+  $.fn.extend({
+
+    filtered: function(facet, value, strictFilter) {
+
+      var collection = collections[this.selector || this];
+
+      if(!collection) { console.log("invalid collection"); return; }
+      collection.facets = collection.facets || {};
+      collection.facets[facet] = value;
+
+      var selector = collection.filters[facet];
+      if(!selector) { console.log("invalid filter"); return; }
+
+      $(this).each(function() {
+
+        var dataAttribute, attrAttribute, attributeValue = "";
+
+        var filters = $(this).data("filters") || {};
+
+        if(dataAttribute = selector.match(dataMatch)) {
+          var attr = dataAttribute[1];
+          attributeValue = ($(this).data(attr)||"")
+        } else if(attrAttribute = selector.match(attrMatch)) {
+          var attr = dataAttribute[1];
+          attributeValue= ($(this).attr(attr)||"")
+        } else {
+          var $child = $(this).find(selector);
+
+          if($child.first().is("input")) {
+            attributeValue = $child.val();
+          } else if($child.length > 0) {
+            attributeValue = $child.map(function() { return $(this).text(); }).get().join(" ");
+          }
+        }
+
+        if(!value) {
+          filters[facet] = true;
+        } else if(strictFilter) {
+          filters[facet] = attributeValue == value
+        } else {
+          filters[facet] =  attributeValue.includes(value)
+        }
+
+        $(this).data("filters",filters);
+      });
+
+      this.updateCollection();
+
+      return $(this.selector || this);
+    },
+
+    updateCollection: function() {
+
+      var collection = collections[this.selector || this];
+
+      $(this).each(function() {
+        var $this = $(this);
+        var filters = $this.data("filters") || {};
+        var visible = true;
+        $.each(filters,function(key,value) {
+          if(!value) { visible = false; }
+        });
+
+        var existing = $this.data("collectionVisible");
+        if(existing === undefined) { existing = true; }
+
+        if(existing != visible) {
+          if(!visible) { 
+            if(collection.hide) {
+              collection.hide($this);
+            } else {
+              $this.hide();
+            }
+          } else {
+            if(collection.show) {
+              collection.show($this);
+            } else {
+              $this.show();
+            }
+          }
+        }
+
+        $this.data("collectionVisible",visible);
+
+      });
+
+      if(collection.update) {
+        collection.update();
+      }
+    },
+
+    collection: function(options) {
+      var selector = this.selector || this;
+      
+      collections[selector] = options;
+      return $(selector);
+    }
+  })
+
+
+})(window);
